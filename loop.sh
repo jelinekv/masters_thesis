ec8b3c6932fddbf2e346743d010d010b4d335f68
Vitek <xjeline1@fi.muni.cz>
2025-01-20 23:52:55 +0000

    Changes in scripts, stages fixed up-to 4. Started a new stage with chosen squares being pullbacks.

--- loop.sh
+++ loop.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-status=$(stat main.tex)
+status=$(stat main.tex | fgrep Modify)
 
 while true; do
-	sleep 30
-	if [ "$status" != "$(stat main.tex)" ]; then
+	sleep 3
+	if [ "$status" != "$(stat main.tex | fgrep Modify)" ]; then
 		#echo unequal
 		./go.sh
-		status=$(stat main.tex)
+		status=$(stat main.tex | fgrep Modify)
 	fi
 done &
 
