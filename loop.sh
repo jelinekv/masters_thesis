eb8cc9a6eb24b6d447fb7bcf3922319153a1a19a
Vit <139985934+jelinekv@users.noreply.github.com>
2025-01-28 17:33:51 +0000

    Small change in loop.sh

--- loop.sh
+++ loop.sh
@@ -5,9 +5,8 @@ status=$(stat main.tex | fgrep Modify)
 while true; do
 	sleep 3
 	if [ "$status" != "$(stat main.tex | fgrep Modify)" ]; then
-		#echo unequal
-		./go.sh
 		status=$(stat main.tex | fgrep Modify)
+		./go.sh
 	fi
 done &
 
